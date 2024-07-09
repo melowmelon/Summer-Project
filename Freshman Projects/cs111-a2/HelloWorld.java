9ba05522847a44bc6ca8199a96776199af469e34
melowmelon <125148326+melowmelon@users.noreply.github.com>
2024-07-09 12:30:55 -0400

    Create HelloWorld.java

---
+++ Freshman Projects/cs111-a2/HelloWorld.java
@@ -0,0 +1,23 @@
+/*************************************************************************
+ *  Compilation:  javac HelloWorld.java
+ *  Execution:    java HelloWorld
+ *
+ *  @author: Merin Ashokkumar, ma1945@scarletmail.rutgers.edu, ma1945
+ *
+ *  Prints "Hello, World". By tradition, this is everyone's first program.
+ *
+ *  % java HelloWorld
+ *  Hello, World
+ *
+ *************************************************************************/
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+	// WRITE YOUR CODE HERE
+    System.out.println("Hello, World");
+    }
+
+}
+
